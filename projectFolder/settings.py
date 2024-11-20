a135fc8b9242b22f336774efa5aab5f0c0480d05
d0bledore <verime@duck.com>
2024-11-20 17:31:00 +0100

    Set postgres as default db

--- projectFolder/settings.py
+++ projectFolder/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -69,10 +70,9 @@ WSGI_APPLICATION = 'projectFolder.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
