d767b46110d34f3d7e54c418cea1c0c502502c24
d0bledore <verime@duck.com>
2024-11-20 16:39:32 +0100

    Add Blog post model

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_index, name='blog_index'),
+    path('posts/', views.blog_post, name='posts'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def blog_index(request):
-    return HttpResponse("You are viewing the blog index!")
+# Import model Blog
+from .models import Blog
 
+def blog_index(request):
+    return render(request, 'blog/index.html')
+    
+
+def blog_post(request):
+    posts = Blog.objects.all()
+    content = {"posts": posts}
+    return render(request, 'blog/posts.html', content)
