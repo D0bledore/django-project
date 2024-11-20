743fb6031a8cf9e7d858d2dd40733ed53ae48b42
d0bledore <verime@duck.com>
2024-11-20 13:22:50 +0100

    Add templates for contact and about

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.core_index, name='core_index'),
+    path('about/', views.core_about, name='core_about'),
+    path('contact/', views.core_contact, name='core_contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def core_index(request):
     return render(request, 'core/index.html')
+
+
+def core_about(request):
+    return render(request, 'core/about.html')
+
+
+def core_contact(request):
+    return render(request, 'core/contact.html')
+
