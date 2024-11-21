72d061e78996a5a4f0d4ab064256932bf8134532
d0bledore <verime@duck.com>
2024-11-21 16:32:49 +0100

    Update BlogPost Model

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Blog
+from django.contrib.auth import get_user_model
+
+# Import models to register
+from .models import BlogPost
 
 # Register your models here.
 
-admin.site.register(Blog)
+User = get_user_model()
+
+class BlogPostAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        # Always set the author to the current admin user
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
+
+admin.site.register(BlogPost, BlogPostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Import model Blog
-from .models import Blog
+from .models import BlogPost
 
 def blog_index(request):
     return render(request, 'blog/index.html')
     
 
 def blog_post(request):
-    posts = Blog.objects.all()
+    posts = BlogPost.objects.all()
     content = {"posts": posts}
     return render(request, 'blog/posts.html', content)
+
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            # Automatically set the author to the current logged-in user
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'create_post.html', {'form': form})
