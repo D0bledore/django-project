b10bfedb59b47b363bb0e1bd735f76e94f774de5
d0bledore <verime@duck.com>
2024-11-24 22:43:39 +0100

    Create views to render profile templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+# Import models
+from blog.models import BlogPost
+
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return redirect('profile', user_id=self.request.user.id)
+
 
 def register(request):
     if request.method == 'POST':
@@ -21,7 +35,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('index')  # Redirect to the index page after login
+            return redirect('profile')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
@@ -31,3 +45,30 @@ def logout_view(request):
         logout(request)
         return redirect('index')  # Redirect to the index page after logout
     return render(request, 'accounts/logout.html')
+
+
+@login_required
+def profile(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    posts = BlogPost.objects.filter(author=user)
+    return render(request, 'accounts/profile.html', {'posts': posts, 'profile_user': user})
+    
+
+@login_required
+def view_posts(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    posts = BlogPost.objects.filter(author=user)
+    return render(request, 'blog/user_posts.html', {'posts': posts, 'profile_user': user})
+
+@login_required
+def manage_posts(request):
+    posts = BlogPost.objects.filter(author=request.user)
+    return render(request, 'blog/manage_posts.html', {'posts': posts})
+
+
+@login_required
+def publish_post(request, post_id):
+    post = get_object_or_404(BlogPost, id=post_id, author=request.user)
+    post.is_published = True
+    post.save()
+    return redirect('user_posts')
