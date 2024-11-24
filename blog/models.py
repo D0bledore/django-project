27014b0b670505eca53e4766e8263a4feff1a3a7
d0bledore <verime@duck.com>
2024-11-24 15:34:30 +0100

    Add is_published table to BlogPost

--- blog/models.py
+++ blog/models.py
@@ -12,9 +12,10 @@ class BlogPost(models.Model):
     condition = models.IntegerField(default=5)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     sale_handling = models.CharField(max_length=100, default='pickup')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
