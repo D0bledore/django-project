db10ec076114bd0766fd22c77b5e57141542f307
d0bledore <verime@duck.com>
2024-11-27 20:46:27 +0100

    Add feature to add upload and display images

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,4 @@ from .models import BlogPost
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = ['title', 'content', 'category', 'condition', 'price', 'sale_handling', 'location']
+        fields = ['title', 'content', 'category', 'condition', 'price', 'sale_handling', 'location', 'image']
--- blog/views.py
+++ blog/views.py
@@ -17,28 +17,16 @@ def blog_post(request):
 
 def create_post(request):
     if request.method == 'POST':
-        form = BlogPostForm(request.POST)
+        form = BlogPostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
-            if request.user.is_authenticated:
-                post.author = request.user
-                post.is_published = False  # Post is not published immediately
-                post.save()
-                return redirect('user_posts', user_id=request.user.id)
-            else:
-                # Handle unauthenticated user
-                email = request.POST.get('email')
-                if email:
-                    post.email = email
-                    post.is_published = False
-                    post.save()
-                    # Send email to user with a link to complete registration
-                    return redirect(reverse('register'))
-                else:
-                    form.add_error(None, 'Email is required for guest users.')
+            post.author = request.user
+            post.save()
+            return redirect('user_posts', user_id=request.user.id)
     else:
         form = BlogPostForm()
     return render(request, 'blog/create_post.html', {'form': form})
+
 
 
 def post_detail(request, post_id):
