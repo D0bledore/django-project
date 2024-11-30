2f08e06ad6449e2e9026e62efab02b1309dec6ff
d0bledore <verime@duck.com>
2024-11-30 02:56:30 +0100

    Adjust blog model and template to not use cloudinary

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 
 
 
@@ -17,7 +16,7 @@ class BlogPost(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    image = CloudinaryField('image', null=True, blank=True)
+    image = models.ImageField(upload_to='images/')
     location = models.CharField(max_length=100, default='')
 
     def __str__(self):
