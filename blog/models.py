f547651182d436d8e9ad000e8e1e71a8221bc7c9
d0bledore <verime@duck.com>
2024-12-18 11:17:28 +0100

    Remove default values from blogpost model

--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from storages.backends.s3boto3 import S3Boto3Storage
 
+User = get_user_model()
 
 
 # Create your models here.
 class BlogPost(models.Model):
-    title = models.CharField(max_length=100, default='Untitled')
-    content = models.TextField(default='No description provided.')
-    category = models.CharField(max_length=100, default='Miscellaneous')
-    condition = models.IntegerField(default=5)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    sale_handling = models.CharField(max_length=100, default='pickup')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.CharField(max_length=100)
+    condition = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    sale_handling = models.CharField(max_length=100)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=False)
     image = models.ImageField(storage=S3Boto3Storage(), upload_to='media/')
-    location = models.CharField(max_length=100, default='')
+    location = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
