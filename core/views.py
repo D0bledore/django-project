7376fbcf916caeb94cbf729f1c0493e3297d1051
d0bledore <verime@duck.com>
2024-12-20 07:00:35 +0100

    Add comments for readability

--- core/views.py
+++ core/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 
 
-# Create your views here.
+# View to render the core index page
 def core_index(request):
     return render(request, 'core/index.html')
 
-
+# View to render the core contact page
 def core_contact(request):
     if request.method == 'POST':
         messages.success(request, 'Your message has been sent successfully!')
