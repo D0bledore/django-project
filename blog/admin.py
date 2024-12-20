339b89440ef4edcc4a06383b420dbac825433f21
d0bledore <verime@duck.com>
2024-12-20 19:17:14 +0100

    Create costum admin class for managing comments

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 
 # Import models to register
-from .models import BlogPost
+from .models import BlogPost, Comment
 
 # Get the user model
 User = get_user_model()
@@ -15,5 +15,18 @@ class BlogPostAdmin(admin.ModelAdmin):
         obj.author = request.user
         super().save_model(request, obj, form, change)
 
+# Define a custom admin class for Comment
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('blog_post', 'author', 'content', 'active')
+    actions = ['deactivate_comments']
+
+    def deactivate_comments(self, request, queryset):
+        queryset.update(active=False)
+    deactivate_comments.short_description = "Deactivate selected comments"
+ 
+
 # Register the BlogPost model with the custom admin class
 admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(Comment, CommentAdmin)
+
+
